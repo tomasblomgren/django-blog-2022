a2adba0038d2f4b69bd4d530887e95b3356afa36
tomasblomgren <Tomas.blomgren96@gmail.com>
2022-11-07 17:45:46 +0000

    deploy commit 2

--- codestar/settings.py
+++ codestar/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['tomasblomgren.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['codestar-2022-django-blog.herokuapp.com', 'localhost']
 
 
 # Application definition
