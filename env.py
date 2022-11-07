e222295b8deb85a8d50ee72e7d6555167a40b173
tomasblomgren <Tomas.blomgren96@gmail.com>
2022-11-07 12:35:41 +0000

    changing database and connecting to heroku

---
+++ env.py
@@ -0,0 +1,4 @@
+import os
+
+os.environ["DATABASE_URL"] = "postgres://ohzilcgmqwqpcc:91de71eab0ee3a13b233ed79de27c1f551d5a0f6a05217e4ab76f66fd8f1743a@ec2-52-212-228-71.eu-west-1.compute.amazonaws.com:5432/d8m86rk6bc1o6c"
+os.environ["SECRET_KEY"] = "secretkey1500"
